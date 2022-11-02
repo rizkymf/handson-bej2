b052abe1206682f79d06f528c95f978ff902613a
rizkymf <rizky.mochamadf@gmail.com>
2022-11-02 22:02:26 +0700

    add service and crud

--- src/test/java/org/binar/chapter4/repository/MahasiswaRepositoryTest.java
+++ src/test/java/org/binar/chapter4/repository/MahasiswaRepositoryTest.java
@@ -4,12 +4,57 @@ import org.binar.chapter4.model.Mahasiswa;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
 
 @SpringBootTest
 public class MahasiswaRepositoryTest {
 
     @Autowired
     MahasiswaRepository mahasiswaRepository;
+
+    @Test
+    void getMahasiswaByAngkatan() {
+        // cara 1 pake JPQL
+        List<Mahasiswa> mahasiswaList1 = mahasiswaRepository.findMahasiswaByAngkatan(2022);
+        mahasiswaList1.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+
+        // cara 2 pake @Query
+        List<Mahasiswa> mahasiswaList2 = mahasiswaRepository.findMahasiswaByAngkatanQuery(2022);
+        mahasiswaList2.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+
+        // cara 3 pake @Query tapi pake nativeQuery = true
+        List<Mahasiswa> mahasiswaList3 = mahasiswaRepository.findMahasiswaByAngkatanNative(2022);
+        mahasiswaList3.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+    }
+
+    @Test
+    void testPageableOnMahasiswa() {
+        System.out.println("print mhs page 0");
+        List<Mahasiswa> mahasiswaList0 = mahasiswaRepository.findAllMahasiswa(PageRequest.of(0, 2));
+        mahasiswaList0.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+        System.out.println("-----------------");
+        System.out.println("print mhs page 1");
+        List<Mahasiswa> mahasiswaList1 = mahasiswaRepository.findAllMahasiswa(PageRequest.of(1, 2));
+        mahasiswaList1.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+        System.out.println("-----------------");
+        System.out.println("print mhs page 2");
+        List<Mahasiswa> mahasiswaList2 = mahasiswaRepository.findAllMahasiswa(PageRequest.of(2, 2));
+        mahasiswaList2.forEach(mhs -> {
+            System.out.println(mhs.getNama());
+        });
+    }
 
     @Test
     void insertMahasiswa() {
@@ -22,5 +67,10 @@ public class MahasiswaRepositoryTest {
 
         // save data mahasiswa ke table mahasiswa
         mahasiswaRepository.save(mahasiswa);
+    }
+
+    @Test
+    void testChangeFakultas_storeProcedure() {
+        mahasiswaRepository.changeFakultas1("01", "03");
     }
 }
---
+++ src/test/java/org/binar/chapter4/service/MahasiswaServiceTest.java
@@ -0,0 +1,21 @@
+package org.binar.chapter4.service;
+
+import org.binar.chapter4.model.Mahasiswa;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MahasiswaServiceTest {
+
+    @Autowired
+    IMahasiswaService mahasiswaService;
+
+    @Test
+    void newMahasiswa_alreadyRegistered() {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.setNama("Udin Samsyudin");
+        Assertions.assertThrows(Exception.class, () -> mahasiswaService.newMahasiswa(mahasiswa));
+    }
+}
