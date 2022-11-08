36b220e657810aefa78fbc742ddc17f43494ddd8
rizkymf <rizky.mochamadf@gmail.com>
2022-11-08 21:48:05 +0700

    rest template

---
+++ src/main/java/org/binar/chapter4/web/DemoRestTemplate.java
@@ -0,0 +1,27 @@
+package org.binar.chapter4.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class DemoRestTemplate {
+    public void testRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://catfact.ninja/fact";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        System.out.println(response);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode json = objectMapper.readTree(response.getBody());
+            json.get("fact");
+        } catch(Exception e) {
+
+        }
+
+    }
+}
---
+++ src/test/java/org/binar/chapter4/web/DemoRestTemplateTest.java
@@ -0,0 +1,17 @@
+package org.binar.chapter4.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoRestTemplateTest {
+
+    @Autowired
+    DemoRestTemplate demoRestTemplate;
+
+    @Test
+    void test1() {
+        demoRestTemplate.testRestTemplate();
+    }
+}
